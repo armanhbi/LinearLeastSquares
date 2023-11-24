5eff88b647c970bb599d61b7512963bffc1aadf2
Arman H <arman.habibi@tum.de>
2023-11-24 17:21:13 +0100

    Created basic structure for program/algorithm

---
+++ src/LLS.java
@@ -0,0 +1,7 @@
+public class LLS {
+    public static Polynom createPolynom(Point[] points) {
+        // create A and b
+        // A^T A x = A^T b -> calculate for x
+        return null;
+    }
+}
